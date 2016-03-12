d19d6fd64109a878477867d60ba62b6a9eec27da
brenna <info@brennaobrien.com>
2016-03-12 15:56:31 -0500

    initial commit

---
+++ _js/script.js
@@ -0,0 +1,14 @@
+// SCRIPT.JS - site wide scripts
+//=========================================================
+
+//fix ipad zoom bug on orientation change
+//---------------------------------------------------------
+if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+    var viewportmeta = document.querySelector('meta[name="viewport"]');
+    if (viewportmeta) {
+        viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0';
+        document.body.addEventListener('gesturestart', function () {
+            viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
+        }, false);
+    }
+}
